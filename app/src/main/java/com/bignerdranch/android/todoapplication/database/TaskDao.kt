08293150b9a0c843095d630a0c2ec8240846b057
yui <yuineor@gmail.com>
2024-12-14 00:11:24 +0700

    create sql in TaskDao

---
+++ app/src/main/java/com/bignerdranch/android/todoapplication/database/TaskDao.kt
@@ -0,0 +1,25 @@
+package com.bignerdranch.android.todoapplication.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+interface TaskDao {
+    @Insert
+    suspend fun insert(task: Task)
+
+    @Delete
+    suspend fun delete(task: Task)
+
+    @Update
+    suspend fun update(task: Task)
+
+    @Query(value = "DELETE FROM task_table")
+    suspend fun deleteAll()
+
+    @Query(value = "SELECT * FROM task_table ORDER BY timestamp DESC")
+    fun getAllTasks(): LiveData<List<Task>>
+
+}
