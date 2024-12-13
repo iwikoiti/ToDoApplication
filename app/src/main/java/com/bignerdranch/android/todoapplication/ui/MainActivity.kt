2507cf3472366eab31a4ba0df758083fdda354b5
yui <yuineor@gmail.com>
2024-12-14 00:03:40 +0700

    configure internal structure and add data class Task

--- app/src/main/java/com/bignerdranch/android/todoapplication/ui/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/todoapplication/ui/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.bignerdranch.android.todoapplication
+package com.bignerdranch.android.todoapplication.ui
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.bignerdranch.android.todoapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
