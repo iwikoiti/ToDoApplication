b1f9852025313fb2398c1e259d9772891e80bdf2
yui <yuineor@gmail.com>
2024-12-13 23:57:53 +0700

    add dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,20 +1,28 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.ksp)
+    alias(libs.plugins.kotlin.parcelize)
+    alias(libs.plugins.navigation.safeargs)
 }
 
 android {
-    namespace = "com.bignerdranch.android.todoapplication"
-    compileSdk = 34
+    namespace = "com.bignerdranch.android.todoapp"
+    compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.bignerdranch.android.todoapplication"
+        applicationId = "com.bignerdranch.android.todoapp"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildFeatures{
+        //noinspection DataBindingWithoutKapt
+        dataBinding = true
     }
 
     buildTypes {
@@ -36,8 +44,24 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
+
+    implementation(libs.androidx.navigation.fragment)
+    implementation(libs.androidx.navigation.ui)
+
+    implementation(libs.androidx.room)
+    implementation(libs.androidx.room.runtime)
+    annotationProcessor(libs.androidx.room.compiler)
+    ksp(libs.androidx.room.compiler)
+
+    implementation(libs.androidx.lifecycle.viewmodel)
+    implementation(libs.androidx.lifecycle.livedata)
+    implementation(libs.androidx.lifecycle.runtime)
+
+    implementation(libs.kotlin.stdlib)
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.android)
+
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
